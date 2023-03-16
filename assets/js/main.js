6408748df296f50afb3f453e102dc690337b6666
fabiodelli <ntqdel@hotmail.com>
2023-03-16 09:55:16 +0100

    with for cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const list = ["pane","carote","carta","acqua","olio","inslata"]
 
-let i = 0
+/* let i = 0
 
 while (i < list.length) {
     var item = list[i];
@@ -8,4 +8,11 @@ while (i < list.length) {
     li.textContent = item;
     document.getElementById('ul').appendChild(li);
     i++;
-  }           
+  }            */
+
+  for(let i = 0;i< list.length;i++){
+    var item = list[i];
+    var li = document.createElement('li');
+    li.textContent = item;
+    document.getElementById('ul').appendChild(li);
+  }
